25eb5cca8bd1979df921d9ba0616b20b6a0f0ba3
Raphael <38656432+Raphik1996@users.noreply.github.com>
2018-05-03 21:30:10 +0300

    Add files via upload

---
+++ Сalculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+    boolean a = 5 + 1 == 6;
+        if (a) {
+            System.out.println("5+1=6");
+        }
+    boolean b = 8 - 2 == 6;
+        if (b) {
+            System.out.println("8-2=6");
+        }
+    boolean c = 2 * 5 == 10;
+        if (c) {
+            System.out.println("2*5=10");
+        }
+    boolean d = 27 / 3 == 9;
+        if (d) {
+            System.out.println("27/3=9");
+        }
+    boolean e = 20 * 5 == 100;
+        if (e) {
+            System.out.println("20*5=100");
+        } 
+    boolean f = 5 % 10 == 5;
+        if (f) {
+            System.out.println("5%10=5");
+
+        }
+    }
+}
